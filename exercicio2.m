260fcd6646b6cc8be716519ec27402707437dd18
luizamourmouris <luizamourmouris@gmail.com>
2025-11-28 13:09:15 -0300

    Update exercicio2.m

--- exercicio2.m
+++ exercicio2.m
@@ -10,26 +10,10 @@ pkg load optim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-# X = [x, y]
-C = [10; 2*NG]
-NG = 2
-NG = 2 ;
+% digite seu codigo aqui
 
-# A*X <= b
-A = [-0.3, -0.2;
-     -0.2, -0.25;
-     -0.25, -0.3];
-c = [10; 2*NG];
 
-b = [-15;
-A = [-0.30, -0.20;  
-     -0.20, -0.25; 
-     -0.25, -0.30]; 
-
-b = [-15; 
-     -10;
-     -12];
-
+% mantenha essas duas linhas finais
 blendA = x(1);
 blendB = x(2);
 
